cc2d18295a1e8808c4184f2d5e414213e56f7c2d
CeereMugambi <ceeremugambi@gmail.com>
2022-09-09 04:59:58 -0700

    Printing using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h> /*Header*/
+/**
+ *main is the entry point with no arguments
+ *puts printinto standard output
+ *Return o to whoever calls it
+*/
 
-int main (void){
+int main (void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
