e4f3f2d2e04bd949511f452b91be3f12f53274e2
CeereMugambi <ceeremugambi@gmail.com>
2022-09-12 11:13:07 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,24 +11,26 @@ int main(void)
 	int c;
 	int d = 0;
 
-	while (d < 10)
+	while (d <10)
 	{
-		if (d != c && d < c)
+		c = 0;
+		while (c < 10)
 		{
-			putchar('0' + d);
-			putchar('0' + c);
-
-			if (c +  d != 17)
+			if (d != c && d < c)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
+			c++;
 		}
-
-		c++;
+		d++;
 	}
-
-	d++;
+	putchar('\n');
+	return (0);
 }
-putchar('\n');
-return (0);
