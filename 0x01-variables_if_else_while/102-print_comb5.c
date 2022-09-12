d9da9fb6c5a0d8a49e18dd98c3719284193a274e
CeereMugambi <ceeremugambi@gmail.com>
2022-09-12 11:38:41 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -22,7 +22,7 @@ int main(void)
 			putchar(second_nums / 10 + '0');
 			putchar(second_nums % 10 + '0');
 
-			if (first_nums == 9 && second_nums == 99)
+			if (first_nums == 98 && second_nums == 99)
 			{
 				break;
 			}
