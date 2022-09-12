e2592c17d142831377d3dd1b07859f31a612ca45
CeereMugambi <ceeremugambi@gmail.com>
2022-09-12 11:01:05 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: This program prints all possible different combinations of 2 digits
+ * Return: 0
+ */
+ int main(void)
+{
+	int c;
+	int d = 0;
+
+	while (d < 10)
+	{
+		if (d != c && d < c)
+		{
+			putchar('0' + d);
+			putchar('0' + c);
+
+			if (c +  d != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+		c++;
+	}
+
+	d++;
+}
+putchar('\n');
+return (0);
