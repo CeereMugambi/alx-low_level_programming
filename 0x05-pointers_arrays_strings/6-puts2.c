320cb9bcf8e62c2be121c9b0f04da6c480e9fab0
CeereMugambi <ceeremugambi@gmail.com>
2022-09-20 06:37:23 -0700

    Function to print characters of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts - Prints all characters of a string
+ * @str: pointer to an int that wil be updated
+ *
+ * Return: Void when output is correct
+ */
+
+Void puts2(char *str)
+{
+	int i;
+
+	(for i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar (str[i]);
+	}
+
+	_putchar('\n');
+}
