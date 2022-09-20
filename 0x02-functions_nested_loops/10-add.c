5f7582c1a3421642cd17f95145959998c93e863b
CeereMugambi <ceeremugambi@gmail.com>
2022-09-20 12:42:29 -0700

    Prora, that adds two integers and return the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add two integers 
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int res = a + b;
+
+	return (res);
+}
