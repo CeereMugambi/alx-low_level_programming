323a6ca3822cfb558f5d911550ecf36ad08950b5
CeereMugambi <ceeremugambi@gmail.com>
2022-09-20 10:54:14 -0700

    Function to count remainder days in a year of specific date

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,7 +15,7 @@ void print_remaining_days(int month, int day, int year)
 	 * by 100 and 400 by 4 evenly
 	 */
 
-	if ((year % 100 == 0 && year % 400 == 0) | (year % 4 == 0))
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month > 2 && day >= 60)
 		{
@@ -23,9 +23,8 @@ void print_remaining_days(int month, int day, int year)
 		}
 
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n". 366 - day );
+		printf("Remaining days: %d\n", 366 - day );
 	}
-
 	else
 	{
 		if (month == 2 && day == 60)
