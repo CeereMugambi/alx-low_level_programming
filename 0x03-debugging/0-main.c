a38da6a7d8e22371b701574b2e664f6d00ea47c3
CeereMugambi <ceeremugambi@gmail.com>
2022-09-20 08:16:32 -0700

    Task0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test program that prints if integer is 
+ * positive or negarive
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98 % 2;
+	positive_or_negative(i);
+
+	return (0);
+}
