83126eb7458a72c4b08259dabb8146c126be056b
CeereMugambi <ceeremugambi@gmail.com>
2022-09-20 07:52:40 -0700

    Function for generating random password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/** 
+ * Main - main block 
+ * function to generate random passwd
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	srand(time(0));
+	num = rand();
+	printf("%i\n" , num);
+
+	return (o);
+}
