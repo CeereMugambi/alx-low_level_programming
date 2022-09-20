9034920b27502a8d303348165ef302e9969eb938
CeereMugambi <ceeremugambi@gmail.com>
2022-09-20 06:24:14 -0700

    Function t print a string in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * rev_string -Prints a revere string
+ * @s: Pointer to int that will be updated
+ *
+ * Return: Void meaning output is correct
+ */
+
+void rev_string(char *s)
+{
+	char *start, *end, c;
+	int i, count;
+	int len = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		len ++
+	}
+
+	count = len;
+	start = s;
+	end = s;
+
+	for (i = 0, i < count - 1; i++)
+	{
+		end++;
+	}
+
+	for (i = 0; i < count / 2; i++)
+	{
+		c = *end;
+		*end = *start;
+		*start = c;
+
+		start++;
+		end--;
+	}
+}
