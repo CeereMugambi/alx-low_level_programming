22eec341fc9aa51c22f484b516a6a62c0c36a731
CeereMugambi <ceeremugambi@gmail.com>
2022-09-22 13:09:51 -0700

    Function to check for a digit between 0 - 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+/**
+ * _isdigit - checks for a digit between 0-9
+ * @c: input
+ * Return: 1 if digit else 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
